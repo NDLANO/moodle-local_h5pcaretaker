b182d27301aefd4713a82d26566afe1fb7abcc6f
Oliver Tacke <o.tacke@posteo.de>
2025-03-24 12:21:24 +0100

    Add French translation

---
+++ lang/fr/local_h5pcaretaker.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Translations for H5P Caretaker plugin.
+ *
+ * @package    local_h5pcaretaker
+ * @copyright  2025 NDLA <explore@ndla.no>
+ * phpcs:ignore moodle.Commenting.FileExpectedTags.LicenseTagInvalid
+ * @license    https://opensource.org/license/mit MIT
+ */
+
+$string['callToAction'] = 'Vérifiez si votre fichier H5P peut être amélioré';
+$string['callToActionDetails'] = 'Découvrez les problèmes d’accessibilité, les informations manquantes et les bonnes pratiques qui peuvent vous aider à améliorer votre contenu H5P.';
+$string['dropzone:fileBeingChecked'] = 'Votre fichier est en cours de vérification';
+$string['dropzone:fileCheckedSuccessfully'] = 'Votre fichier a été vérifié avec succès';
+$string['dropzone:orDragTheFileHere'] = 'ou glissez le fichier ici';
+$string['dropzone:removeFile'] = 'Supprimer le fichier';
+$string['dropzone:uploadH5PFile'] = 'Envoyez votre fichier H5P';
+$string['dropzone:uploadProgress'] = 'Avancement de l’envoi';
+$string["error:checkServerLog"] = "Veuillez consulter les journaux du serveur.";
+$string['error:fileTooLarge'] = 'Le fichier est plus grand que la taille maximale autorisée de %s KB.';
+$string['error:forbidden'] = 'Interdit';
+$string['error:methodNotAllowed'] = 'Méthode non autorisée';
+$string['error:noFileOrTooLarge'] = 'Il semble qu’aucun fichier n’ait été fourni ou qu’il dépasse la taille limite d’envoi de %s KB.';
+$string['error:notAnH5PFile'] = 'Le fichier n’est pas un fichier H5P.';
+$string['error:unknownError'] = 'Quelque chose s’est mal passé, mais je ne sais pas quoi, désolé!';
+$string['expand:collapseAllMessages'] = 'Replier tous les messages';
+$string['expand:expandAllMessages'] = 'Déplier tous les messages';
+$string['filter:allFilteredOut'] = 'All messages have been filtered out by content.';
+$string['filter:contentFilter'] = 'Filtre du type de contenu';
+$string['filter:filterByContent'] = 'Filtrer sur le contenu:';
+$string['filter:reset'] = 'Réinitialiser';
+$string['filter:showAll'] = 'Tout afficher';
+$string['filter:showNone'] = 'Ne rien afficher';
+$string['filter:showSelected'] = 'Différents contenus sélectionnés';
+$string['h5pcaretaker'] = 'H5P Caretaker';
+$string['headline'] = 'Prenez soin de votre H5P';
+$string['languageSelect:a11ySelectLanguage'] = 'Choisir votre langue';
+$string['moodle:extratext_footer'] = 'Pied de page supplémentaire';
+$string['moodle:extratext_footer_desc'] = 'Ajouter des informations supplémentaires dans le pied de page.';
+$string['moodle:extratext_intro'] = 'Introduction supplémentaire';
+$string['moodle:extratext_intro_desc'] = 'Ajoutez des informations d’introduction supplémentaires.';
+$string['moodle:forcelogin'] = 'Visibilité';
+$string['moodle:forcelogin_desc'] = 'Indiquez si la page H5P Caretaker doit être accessible au public ou uniquement aux utilisateurs connectés dont le rôle dispose de la capacité.';
+$string['moodle:needscapability'] = 'Nécessite des droits';
+$string['moodle:public'] = 'Publique';
+$string['pluginname'] = 'NDLA\'s H5P Caretaker';
+$string['report:titleTemplate'] = 'Rapport H5P-Caretaker pour @title';
+$string['results:download'] = 'Télécharger';
+$string['results:filterBy'] = 'Filtrer par';
+$string['results:groupBy'] = 'Grouper par';
+$string['results:hideDetails'] = 'Cacher les détails';
+$string['results:issues'] = 'problèmes';
+$string['results:results'] = 'résultats';
+$string['results:showDetails'] = 'Voir les détails';
+$string['results:totalMessages'] = 'Total des messages';
+$string['site:title'] = 'NDLA\'s H5P Caretaker';
